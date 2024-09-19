7581c1c0e62ac8a85f52e909e0aa3b1993a6c04e
Keerthana T P <tpkeerthana944@gmail.com>
2024-09-20 00:02:12 +0530

    auth

--- middleware.js
+++ middleware.js
@@ -1,6 +1,8 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware()
+export default authMiddleware({
+  publicRoutes:["/api/webhook"]
+})
 
 export const config = {
   // The following matcher runs middleware on all routes
